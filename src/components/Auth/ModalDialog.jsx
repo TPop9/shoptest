ab9d56b2eef3e4cdff10612ed5f50db48dda307a
Toni Poparić <tpoparic@outlook.com>
2022-11-16 14:02:57 +0100

    shoptest3

--- src/components/Auth/ModalDialog.jsx
+++ src/components/Auth/ModalDialog.jsx
@@ -9,7 +9,7 @@ function ModalDialog({ data, setModalData, title }) {
 
   return (
     <Modal className="modal" show={true}>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
 
